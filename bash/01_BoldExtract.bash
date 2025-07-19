4598e585e2e37fec3f3c292ebc66e7d504ca609f
kaanka5312 <kaanka5312@gmail.com>
2025-07-19 13:03:04 +0300

    new

--- bash/01_BoldExtract.bash
+++ bash/01_BoldExtract.bash
@@ -53,7 +53,7 @@ done
 cd /Volumes/HD-B1/Thesis/SES-2_BIDS/derivatives/afni_proc
 
 #for subj in 1 4 5 6 8 13 19 20 23 36 ;do
-for subj in 33 38 39 ;do
+for subj in 1 ;do
 
 subj=${subj}_ses2
 echo ${subj}
