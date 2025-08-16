3462abbeda52bdec6369548f819ff981d4db5def
kaanka5312 <kaanka5312@gmail.com>
2025-08-16 20:05:12 +0300

    new

--- bash/01_BoldExtract.bash
+++ bash/01_BoldExtract.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Below part is for NOT censored data. 
 #=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
 #####  B I P O L A R #####################
 #=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
@@ -98,4 +100,103 @@ cd sub-${subj}.results
 	done
 
 cd /media/kaanka5312/HD-B1/BIDS/derivatives/afni/	
+done
+
+################################
+## C E N S O R E D #############
+################################
+
+# Linux
+cd /Volumes/HD-B1/BPB_proc
+
+for subj in {1..2} {4..9} {10..13} {15..23};do
+
+	echo ${subj}
+	cd sub-${subj}.results
+
+		cd Zscore_data_bandpass
+		# Extracting the BOLD signal and save
+		
+		for ROI in {1..180} {1001..1180};do
+		
+		3dROIstats -quiet \
+		-mask /Users/kaankeskin/projects/MultGroup_WC/REPLICATION/MASK/glasser_${ROI}.nii \
+		rest_Zscore.censored+tlrc \
+		> BOLD_${ROI}_censored.1D
+		done
+
+cd /Volumes/HD-B1/BPB_proc
+done
+
+##### SESSION -1 #####
+
+cd /Volumes/HD-B1/Thesis/SES-1_BIDS/derivatives/afni_proc
+
+for subj in {24..42} ;do
+
+echo ${subj}
+cd sub-${subj}.results
+
+	cd Zscore_data_bandpass
+		# Extracting the BOLD signal and save
+		
+	for ROI in {1..180} {1001..1180};do
+		
+	3dROIstats -quiet \
+	-mask /Users/kaankeskin/projects/MultGroup_WC/REPLICATION/MASK/glasser_${ROI}.nii \
+	rest_Zscore.censored+tlrc \
+	> BOLD_${ROI}_censored.1D
+	done
+
+cd /Volumes/HD-B1/Thesis/SES-1_BIDS/derivatives/afni_proc
+done
+
+##### SESSION -2 #####
+
+cd /Volumes/HD-B1/Thesis/SES-2_BIDS/derivatives/afni_proc
+
+#for subj in 1 4 5 6 8 13 19 20 23 36 33 38 ;do
+for subj in 1 ;do
+subj=${subj}_ses2
+echo ${subj}
+cd sub-${subj}.results
+
+	cd Zscore_data_bandpass
+		# Extracting the BOLD signal and save
+		
+	for ROI in {1..180} {1001..1180};do
+		
+	3dROIstats -quiet \
+	-mask /Users/kaankeskin/projects/MultGroup_WC/REPLICATION/MASK/glasser_${ROI}.nii \
+	rest_Zscore.censored+tlrc \
+	> BOLD_${ROI}_censored.1D
+	done
+
+cd /Volumes/HD-B1/Thesis/SES-2_BIDS/derivatives/afni_proc
+done
+
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+#####  C O N T R O L #####################
+#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=#=
+
+# Linux
+cd /Volumes/HD-B1/BIDS/derivatives/afni/
+
+for subj in {35..53} {55..57} {59..70};do
+
+echo ${subj}
+cd sub-${subj}.results
+
+	cd Zscore_data_bandpass
+	# Extracting the BOLD signal and save
+		
+	for ROI in {1..180} {1001..1180};do
+		
+	3dROIstats -quiet \
+	-mask /Users/kaankeskin/projects/MultGroup_WC/REPLICATION/MASK/glasser_${ROI}.nii \
+	rest_Zscore.censored+tlrc \
+	> BOLD_${ROI}_censored.1D
+	done
+
+cd /Volumes/HD-B1/BIDS/derivatives/afni/
 done
